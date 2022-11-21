6cce5d3da48879cdef126940aa5529afcff03c9a
August Wester <august.wester@gmail.com>
2022-11-22 00:25:15 +0100

    Update attention.py

--- attention.py
+++ attention.py
@@ -3,13 +3,18 @@ from torch import nn
 from torch.nn import Parameter, LayerNorm
 
 class Attention(nn.Module):
-    def __init__(self, embed_dim, ctx_len, mem_len):
+    def __init__(self, embed_dim, seg_len, mem_len, num_heads):
         super().__init__()
-        self.w_q = nn.Linear(embed_dim, embed_dim)
-        self.w_ke = nn.Linear(embed_dim, embed_dim)
-        self.w_kr = nn.Linear(embed_dim, embed_dim)
-        self.w_v = nn.Linear(embed_dim, embed_dim)
-        self.mlp = nn.Linear(embed_dim, embed_dim)
+        
+        # should these have identical rows???
+        self.u = Parameter(torch.randn(num_heads, seg_len, embed_dim))
+        self.v = Parameter(torch.randn(num_heads, seg_len, embed_dim))
+        
+        self.w_q = nn.Linear(embed_dim, num_heads*embed_dim)
+        self.w_ke = nn.Linear(embed_dim, num_heads*embed_dim)
+        self.w_kr = nn.Linear(embed_dim, num_heads*embed_dim)
+        self.w_v = nn.Linear(embed_dim, num_heads*embed_dim)
+        self.mlp = nn.Linear(num_heads*embed_dim, embed_dim)
         self.layer_norm = LayerNorm(embed_dim)
         
         self.pos_ff = nn.Sequential(
@@ -18,32 +23,42 @@ class Attention(nn.Module):
             nn.Linear(4*embed_dim, embed_dim)
         )
         
-        self.mem = torch.zeros(mem_len, embed_dim, requires_grad=False)
+        self.mem_len = mem_len
+        self.mem = torch.empty(0, 0, embed_dim, requires_grad=False)
         
-        self.u = Parameter(torch.randn(ctx_len, embed_dim))
-        self.v = Parameter(torch.randn(ctx_len, embed_dim))
-        
-        r = self.get_sinusoid_pos_encoding(ctx_len+mem_len, embed_dim)
-        self.r = torch.flip(r, dims=(0,))
+        pos = self.get_sinusoid_pos_encoding(seg_len+mem_len, embed_dim)
+        self.pos = torch.flip(pos, dims=(0,))
     
-    def forward(self, x, h):
-        h = torch.cat((h, x), dim=1)
-        q = self.w_q(x)
-        k = self.w_ke(h)
-        v = self.w_v(h)
+    def forward(self, x, mem):
+        # concat output from previous layer with "memory" from earlier segments
+        h = torch.cat((mem, x), dim=1)
         
-        _, ctx_len, embed_dim = x.shape
-        mem_len = h.shape[1] - ctx_len
+        batch_size, seg_len, embed_dim = x.shape
+        mem_len = h.shape[1] - seg_len
+        total_len = h.shape[1]
         
-        b = q @ self.w_kr(self.r).T
-        b = self.circulant_shift(b, -ctx_len+1) # do we need to tril here or is att.tril fine?
-                
-        att = q @ k.T + b + self.u @ k.T + self.v @ self.w_kr(self.r).T
+        # compute projections of output from previous layer and the memory
+        q = self.w_q(x).reshape(batch_size, -1, seg_len, embed_dim)
+        k = self.w_ke(h).reshape(batch_size, -1, total_len, embed_dim)
+        v = self.w_v(h).reshape(batch_size, -1, total_len, embed_dim)
+        r = self.w_kr(self.pos).reshape(-1, total_len, embed_dim)
+        
+        # compute relative positional encodings
+        b = q @ r.transpose(1, 2)
+        b = self.circulant_shift(b, -seg_len+1)
+        
+        # this is the XL specific way of computing the attention score
+        k = k.transpose(2, 3)
+        att = q @ k + b + self.u @ k + self.v @ r.transpose(1, 2)
         att = att.tril(mem_len) / embed_dim**0.5
         att = torch.softmax(att, dim=-1)
         
-        out = self.layer_norm(self.mlp(att @ v))
-        return self.pos_ff(out)
+        # compute the output of the layer and save to memory
+        out = self.layer_norm(self.mlp(att @ v) + x)
+        out = self.pos_ff(out)
+        self.save_to_memory(out)
+        
+        return out
           
     def get_sinusoid_pos_encoding(self, mem_len, embed_dim):
         pos = torch.arange(mem_len).unsqueeze(1)
@@ -56,22 +71,25 @@ class Attention(nn.Module):
     def circulant_shift(self, x, shift):
         """
         Shifts top row of `x` by `shift`, second row by `shift-1`, etc. This is
-        used when computing the relative positional encoding matrix in linear
-        time (as opposed to quadratic time for the naive solution).
+        used to compute the relative positional encoding matrix in linear time
+        (as opposed to quadratic time for the naive solution).
         
         See Appendix B of the Transformer-XL paper for more details.
         """
-        batch_size, height, width = x.shape
+        batch_size, num_heads, height, width = x.shape
         i = torch.arange(width).roll(shift).unsqueeze(0)
         i = i.flip(1).repeat(1, 2)
-        i = i.unfold(dimension=1, size=width, step=1).flip(-1)
-        i = i.repeat(batch_size, 1, 1)[:, :height]
-        return x.gather(2, i)
+        i = i.unfold(dimension=1, size=width, step=1).flip(-1).unsqueeze(0)
+        i = i.repeat(batch_size, num_heads, 1, 1)[:, :, :height]
+        return x.gather(3, i)
+    
+    def save_to_memory(self, h):
+        self.mem = torch.stack((self.mem, h), dim=1)[:, :self.mem_len]
 
 batch_size = 64
 embed_dim = 512
-ctx_len = 100
-x = torch.randn(batch_size, ctx_len, embed_dim)
+seg_len = 100
+x = torch.randn(batch_size, seg_len, embed_dim)
 h = torch.randn(batch_size, 384, embed_dim)
-att = Attention(embed_dim, ctx_len, mem_len=384)
+att = Attention(embed_dim, seg_len, mem_len=384, num_heads=10)
 out = att(x, h)
