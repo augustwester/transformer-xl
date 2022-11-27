27dc105ac00db033a4576e0591ab2cc548e62071
August Wester <august.wester@gmail.com>
2022-11-27 16:40:45 +0100

    Update attention.py

--- attention.py
+++ attention.py
@@ -2,24 +2,22 @@ import torch
 from torch import nn
         
 class MultiHeadAttention(nn.Module):
-    def __init__(self, model_dim, embed_dim, seg_len, mem_len, num_heads, device):
+    def __init__(self, model_dim, embed_dim, mem_len, num_heads, R, device):
         super().__init__()
         
+        self.R = R
         self.mem_len = mem_len
         self.embed_dim = embed_dim
         self.device = device
         
-        self.u1 = nn.Parameter(torch.randn(num_heads, 1, embed_dim))
-        self.u2 = nn.Parameter(torch.randn(num_heads, 1, embed_dim))
+        self.u1 = nn.Parameter(torch.Tensor(1, num_heads, 1, embed_dim))
+        self.u2 = nn.Parameter(torch.Tensor(1, num_heads, 1, embed_dim))
         
         self.w_q = nn.Linear(model_dim, num_heads*embed_dim, bias=False)
         self.w_kv = nn.Linear(model_dim, 2*num_heads*embed_dim, bias=False)
         self.w_r = nn.Linear(model_dim, num_heads*embed_dim, bias=False)
         self.mlp = nn.Linear(num_heads*embed_dim, model_dim, bias=False)
         self.layer_norm = nn.LayerNorm(model_dim)
-        
-        pos = self.get_sinusoid_pos_encoding(seg_len+mem_len, embed_dim).to(device)
-        self.pos = torch.flip(pos, dims=(0,))
     
     def forward(self, x, mem, att_mask):
         # concat output from previous layer with "memory" from earlier segments
@@ -32,57 +30,35 @@ class MultiHeadAttention(nn.Module):
         # compute projections of input and memory embeddings
         q = self.w_q(x).view(batch_size, -1, seg_len, embed_dim)
         kv = self.w_kv(h).view(2*batch_size, -1, total_len, embed_dim)
+        r_emb = self.w_r(self.R[-total_len:]).view(1, -1, total_len, embed_dim)
         k, v = kv.chunk(2, dim=0)
-        k = k.transpose(2, 3) # only using transposed k below
         
-        # relative distance between two tokens is max total_len
-        pos = self.pos[-total_len:]
-        r = self.w_r(pos).view(-1, total_len, embed_dim)
+        # the "XL specific" way of computing the pre-softmax attention score
+        AC = torch.einsum("bhid,bhjd->bhij", q + self.u1, k)
+        BD = torch.einsum("bhid,bhjd->bhij", q + self.u2, r_emb)
+        BD = self.circulant_shift(BD, -seg_len+1)
         
-        # compute relative positional encodings
-        b = q @ r.transpose(1, 2)
-        b = self.circulant_shift(b, -seg_len+1)
-        
-        # u1 and u2 should be identical for all query vectors
-        u1 = self.u1.repeat(1, seg_len, 1)
-        u2 = self.u2.repeat(1, seg_len, 1)
-        
-        # this is the XL specific way of computing the attention score
-        #att_mask = att_mask.unsqueeze(1).unsqueeze(-1).repeat(1,10,1,total_len)
-        att_score = q @ k + b + u1 @ k + u2 @ r.transpose(1, 2)
-        #att_score = att_score * att_mask
+        # computing the attention scores
+        att_score = AC + BD
         att_score = att_score.tril(mem_len) / embed_dim**0.5
         att_score[att_score == 0] = float("-inf")
         att_score = torch.softmax(att_score, dim=-1)
         
-        # compute output and save to memory
+        # compute output
         att = (att_score @ v).view(batch_size, seg_len, -1)
         return self.layer_norm(self.mlp(att) + x)
-          
-    def get_sinusoid_pos_encoding(self, mem_len, embed_dim):
-        """
-        Standard sinusoid positional encoding method outlined in the original
-        Transformer paper. In this case, we use the encodings not to represent
-        each token's position in a sequence but to represent the distance
-        between two tokens (i.e. as a *relative* positional encoding).
-        """
-        pos = torch.arange(mem_len).unsqueeze(1)
-        enc = torch.arange(embed_dim).float()
-        enc = enc.unsqueeze(0).repeat(mem_len, 1)
-        enc[:, ::2] = torch.sin(pos / 10000**(2*enc[:, ::2]/embed_dim))
-        enc[:, 1::2] = torch.cos(pos / 10000**(2*enc[:, 1::2]/embed_dim))
-        return enc
-    
+              
     def circulant_shift(self, x, shift):
         """
         Shifts top row of `x` by `shift`, second row by `shift-1`, etc. This is
         used to compute the relative positional encoding matrix in linear time
-        (as opposed to quadratic time for the naive solution).
+        (as opposed to quadratic time for the naive solution). Note: Right-hand
+        side values are not zeroed out here.
         
         See Appendix B of the Transformer-XL paper for more details.
         """
         batch_size, num_heads, height, width = x.shape
-        i = torch.arange(width).roll(shift).unsqueeze(0).to(device)
+        i = torch.arange(width).roll(shift).unsqueeze(0).to(self.device)
         i = i.flip(1).repeat(1, 2)
         i = i.unfold(dimension=1, size=width, step=1).flip(-1).unsqueeze(0)
         i = i.repeat(batch_size, num_heads, 1, 1)[:, :, :height]
