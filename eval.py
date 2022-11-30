8b9a764f3e006d83a090a04054aad6b53568d46d
August Wester <august.wester@gmail.com>
2022-11-30 12:15:38 +0100

    Update eval.py

--- eval.py
+++ eval.py
@@ -1,26 +1,24 @@
 import torch
 from model import TransformerXL
+from data import gen_dataset
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-saved = torch.load("model.pt")
+saved = torch.load("model.pt", map_location=torch.device("cpu"))
 state_dict, config = saved["state_dict"], saved["config"]
-config.seq_len = 1
-config.mem_len = 20
 
 model = TransformerXL(config, device)
 model.load_state_dict(state_dict)
 model.eval()
 
-num_samples = 100
-seqs = torch.randint(0, 10, size=(num_samples, 20)).to(device)
+seq_len = 20
+num_samples = 1000
+test_data, test_targets = gen_dataset(0, 10, seq_len, num_samples)
 preds = torch.empty(num_samples, 0)
 
 for i in range(20):
-    out = model(seqs) if i == 0 else model(next_token)
+    out = model(test_data) if i == 0 else model(next_token)
     next_token = out.argmax(-1)[:, -1:]
     preds = torch.cat((preds, next_token), dim=-1)
-    
-num_correct = (seqs == preds).prod(1).sum()
-print(f"{num_correct} / {num_samples}")
 
+num_correct = (test_data.sort()[0] == preds).prod(1).sum()
+print(f"{num_correct} / {num_samples}")
