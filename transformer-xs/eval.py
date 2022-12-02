6d68b415d68bbefe566f20266619f25818b59d17
August Wester <august.wester@gmail.com>
2022-12-02 12:50:14 +0100

    Update eval.py

--- transformer-xs/eval.py
+++ transformer-xs/eval.py
@@ -1,24 +1,70 @@
 import torch
+import argparse
 from model import TransformerXL
 from data import gen_dataset
+from tqdm import tqdm
+from math import ceil
+from time import time
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-saved = torch.load("model.pt", map_location=torch.device("cpu"))
-state_dict, config = saved["state_dict"], saved["config"]
+def eval(seq_len, num_digits, num_samples):
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    saved = torch.load("model.pt", map_location=torch.device("cpu"))
+    state_dict, config = saved["state_dict"], saved["config"]
+    
+    config.seg_len = 80
 
-model = TransformerXL(config, device)
-model.load_state_dict(state_dict)
-model.eval()
+    model = TransformerXL(config, device)
+    model.load_state_dict(state_dict)
+    model.eval()
 
-seq_len = 20
-num_samples = 1000
-test_data, test_targets = gen_dataset(0, 10, seq_len, num_samples)
-preds = torch.empty(num_samples, 0)
+    test_data, test_targets = gen_dataset(0, num_digits, seq_len, num_samples)
+    test_data, test_targets = test_data.to(device), test_targets.to(device)
+    batch_size = min(1000, num_samples)
+    num_batches = ceil(num_samples / batch_size)
+    
+    print("Evaluating without memory...")
 
-for i in range(20):
-    out = model(test_data) if i == 0 else model(next_token)
-    next_token = out.argmax(-1)[:, -1:]
-    preds = torch.cat((preds, next_token), dim=-1)
+    model.mem_len = 0
+    preds = torch.empty(0, seq_len).to(device)
+    start = time()
+    for i in tqdm(range(num_batches)):
+        batch_preds = test_data[i*batch_size:(i+1)*batch_size]
+        for k in range(seq_len):
+            out = model(batch_preds)
+            next_token = out.argmax(-1)[:, -1:]
+            batch_preds = torch.cat((batch_preds, next_token), dim=-1)
+        preds = torch.cat((preds, batch_preds[:, -seq_len:]))
+    end = time()
+    num_correct = (test_data.sort()[0] == preds[:, -seq_len:]).prod(1).sum()
+    print(f"Achieved accuracy of {num_correct / num_samples} in {end - start} seconds\n")
 
-num_correct = (test_data.sort()[0] == preds).prod(1).sum()
-print(f"{num_correct} / {num_samples}")
+    print("Evaluating with memory...")
+    
+    model.clear_memory()
+    model.mem_len = seq_len * 2 - 1
+
+    preds = torch.empty(num_samples, seq_len).to(device)
+    start = time()
+    for i in tqdm(range(num_batches)):
+        seqs = test_data[i*batch_size:(i+1)*batch_size]
+        for j in range(seq_len):
+            out = model(seqs) if j == 0 else model(next_token)
+            next_token = out.argmax(-1)[:, -1:]
+            preds[i*batch_size:(i+1)*batch_size, j:j+1] = next_token
+        model.clear_memory()
+    end = time()
+    num_correct = (test_data.sort()[0] == preds).prod(1).sum()
+    print(f"Achieved accuracy of {num_correct / num_samples} in {end - start} seconds")
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--sequence_length", type=int, default=20)
+    parser.add_argument("--num_digits", type=int, default=10)
+    parser.add_argument("--num_samples", type=int, default=5000)
+
+    args = parser.parse_args()
+    seq_len = args.sequence_length
+    num_digits = args.num_digits
+    num_samples = args.num_samples
+    
+    eval(seq_len, num_digits, num_samples)
